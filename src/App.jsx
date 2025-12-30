a72d4da3aa31dea278eaf48e07a9a1f3819ab18b
Kabir Alkasim <codesmartng1@gmail.com>
2025-12-30 07:47:47 +0100

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div style={{padding:20}}>
+      <h1>CodeSmart NG</h1>
+      <p>Software & Training</p>
+      <button onClick={() =>
+        window.open("https://wa.me/2348160932630")
+      }>
+        Enroll via WhatsApp
+      </button>
+    </div>
+  )
+}
+
+export default App;
